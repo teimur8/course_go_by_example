723654a7633131916b98254547a92db42ad8a421
teimur8 <gt720@yandex.ru>
2019-06-02 19:26:20 +0600

    Update main.go
    ++

--- source_code/s02/25_scope/main.go
+++ source_code/s02/25_scope/main.go
@@ -1,40 +1,50 @@
 // File name: ...\s02\25_scope\main.go
 // Course Name: Go (Golang) Programming by Example (by Kam Hojati)
 
+/*
+Глобальный и локальный.
+{ } - создают свой скоп
+*/
+
 package main
 
 import "fmt"
-
+// _ГП - глобальная перемнная
 var g int = 10
 
 // g := 20
 
 func main() {
 
-	fmt.Println(g)
+	fmt.Println(g) // 10
 
 	{
+		// скобки создают свой scope
+		// доступ к гл.пер. есть
 		g := 20
-		fmt.Println(g)
+		fmt.Println(g) // 20
 		f := 2.3
-		fmt.Println(f)
+		fmt.Println(f) // 2.3
 	}
 
 	// fmt.Println(f) //compiler error - undefined 'f'
 
-	fmt.Println(g)
+	fmt.Println(g) // 10
 	g += 1
-
+	fmt.Println(g) // 11
 	{
-		g += 4
-		fmt.Println(g)
+		g += 4 // обращение к _ГП
+		fmt.Println(g) //15
 	}
 
+	fmt.Println(g) // 15
+	
 	sayHello()
 	// fmt.Println(s)		//compiler error - undefined 's'
 }
 
 func sayHello() {
+	// у функци локальный scope
 	fmt.Println("Hello!")
 	fmt.Println(g)
 
